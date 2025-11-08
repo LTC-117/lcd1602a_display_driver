6580c6dc0d10f3917108da56bcddebebc9a5034d
LTC-117 <lucastentoni@gmail.com>
2025-11-08 19:26:20 -0300

    [Exporting the application init module to the header file]

--- Inc/lcd1602a.h
+++ Inc/lcd1602a.h
@@ -2,6 +2,11 @@
 #define __LCD1602A_DISPLAY_DRIVER_H__
 
 
+#include "stm32f1xx_hal.h"
+#include "stm32f1xx_hal_i2c.h"
+
+
+void lcd_driver_init(I2C_TypeDef *i2c_module, uint32_t i2c_speed);
 
 
 #endif

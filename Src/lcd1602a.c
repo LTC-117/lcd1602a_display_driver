e426aab98cfd1f9467a6b725084d1d1900b4191d
LTC-117 <lucastentoni@gmail.com>
2025-11-12 20:46:42 -0300

    [Adding license + minor corrections]

--- Src/lcd1602a.c
+++ Src/lcd1602a.c
@@ -1,14 +1,41 @@
+/** @file lcd1602a.c
+ *
+ * @brief Simple, non-portable STM32F103 driver for integration with the
+ * LCD-1602A Display through an PCF8574T I2C Module.
+ *
+ * @par
+ * Copyright (c) 2025 Lucas Tentoni Costa
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #include "lcd1602a.h"
 
-#include "main.h"
-#include "stm32f1xx_hal.h"
 #include "stm32f1xx_hal_gpio.h"
 #include "stm32f1xx_hal_i2c.h"
-#include "stm32f103xb.h"
-#include <stdint.h>
-
+#include "main.h"
+//#include "stm32f103xb.h"
+//#include <stdint.h>
 
 /* 
+ * @pinout
+ *
  * I2C1_SCL -> PB6 | AF Open Drain
  * I2C1_SDA -> PB7 | AF Open Drain
  *
@@ -17,6 +44,16 @@
  * D = 1 -> display on
  */
 
+
+//static uint8_t lcd_buffer[LCD_BUFFER_SIZE];
+
+
+/*!
+ * @brief Initialize the STM32 board's I2C module
+ * @param i2c_module  The address of the respective I2C Module selected.
+ * @param i2c_speed   The clock speed operation for the selected module.
+ * @retval None
+ */
 void lcd_driver_init(I2C_TypeDef *i2c_module, uint32_t i2c_speed)
 {
     /* --------------- GPIO Initialization --------------- */
@@ -67,4 +104,9 @@ void lcd_driver_init(I2C_TypeDef *i2c_module, uint32_t i2c_speed)
     if (HAL_I2C_Init(&hi2c) != HAL_OK) {
         Error_Handler();
     }
-}
+} /* lcd_driver_init() */
+
+
+void lcd_driver_write(void)
+{
+} /* lcd_driver_init() */

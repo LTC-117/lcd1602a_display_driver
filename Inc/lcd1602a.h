3f55c68b8e351e125914338b2a7ade2614221054
LTC-117 <lucastentoni@gmail.com>
2025-11-12 20:47:43 -0300

    [Adding license + minor corrections]

--- Inc/lcd1602a.h
+++ Inc/lcd1602a.h
@@ -1,9 +1,37 @@
-#ifndef __LCD1602A_DISPLAY_DRIVER_H__
-#define __LCD1602A_DISPLAY_DRIVER_H__
+/** @file lcd1602a.h
+ *
+ * @brief LCD1602A Display device driver for STM32F1XX boards
+ *
+ * @par
+ * Copyright (c) 2025 Lucas Tentoni Costa
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef __LCD1602A_H__
+#define __LCD1602A_H__
 
 
 #include "stm32f1xx_hal.h"
-#include "stm32f1xx_hal_i2c.h"
+
+
+#define LCD_BUFFER_SIZE     32
 
 
 void lcd_driver_init(I2C_TypeDef *i2c_module, uint32_t i2c_speed);
